27c5a874c6db88168133de7e604e3ee4eb54417f
adriangohjw <adriangohjw@gmail.com>
2021-11-13 03:36:18 +0800

    Update tf_generator QueryProcessor to also return normalized tf-idf value

--- tf-idf/tf_generator.py
+++ tf-idf/tf_generator.py
@@ -1,7 +1,7 @@
 import math
 
 class QueryProcessor:
-  def call(self, query):
+  def call(self, query, document_df_dict):
     result = {}
 
     for word in query.split():
@@ -9,20 +9,41 @@ class QueryProcessor:
         result[word] = 0 
       result[word] += 1
     
-    return self.add_term_frequency_log_to_dict(result)
+    return self.__get_scoring(result, document_df_dict)
   
 
-  def add_term_frequency_log_to_dict(self, tf_dict):
+  def __get_scoring(self, tf_dict, document_df_dict):
     result = {}
+
     for key, value in tf_dict.items():
+      tf_log = 1 + math.log(value)
+      tf_idf = tf_log * self.__get_idf_from_document_df_dict_by_key(key, document_df_dict)
       result[key] = {
         'tf': value,
-        'tf-log': 1 + math.log(value)
+        'tf_log': tf_log,
+        'tf_idf': tf_idf
       }
-      
-    return result
+
+    return self.__normalize_scoring(result)
+
   
+  def __get_idf_from_document_df_dict_by_key(self, key, document_df_dict):
+    key_match = document_df_dict.get(key)
+    return key_match['idf'] if key_match is not None else 0
+
+
+  def __normalize_scoring(self, scoring_dict):
+    sum_of_tf_idf_squared = 0
+    for _, value in scoring_dict.items():
+      sum_of_tf_idf_squared += value['tf_idf'] ** 2
+    normalization_factor = 1 / math.sqrt(sum_of_tf_idf_squared)
+    
+    for key, value in scoring_dict.items():
+      scoring_dict[key]['norm'] = value['tf_idf'] * normalization_factor
   
+    return scoring_dict
+
+
 class DocumentProcessor:
   def call(self, cleaned_query, cleaned_text):
     cleaned_text_list = cleaned_text.split()
