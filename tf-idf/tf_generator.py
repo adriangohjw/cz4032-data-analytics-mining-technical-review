c467c28c4d28be9b5b49813ece68bc6d093c1958
adriangohjw <adriangohjw@gmail.com>
2021-11-13 03:10:45 +0800

    Add DocumentProcessor for tf_generator

--- tf-idf/tf_generator.py
+++ tf-idf/tf_generator.py
@@ -21,3 +21,29 @@ class QueryProcessor:
       }
       
     return result
+  
+  
+class DocumentProcessor:
+  def call(self, cleaned_query, cleaned_text):
+    cleaned_text_list = cleaned_text.split()
+    
+    result = {}
+    for term in self.unique_terms(cleaned_query):
+      result[term] = cleaned_text_list.count(term)
+    
+    return self.add_term_frequency_log_to_dict(result)
+
+
+  def unique_terms(self, query):
+    return list(set(query.split()))
+  
+  
+  def add_term_frequency_log_to_dict(self, tf_dict):
+    result = {}
+    for key, value in tf_dict.items():
+      result[key] = {
+        'tf': value,
+        'tf-log': 0 if value == 0 else 1 + math.log(value)
+      }
+      
+    return result
