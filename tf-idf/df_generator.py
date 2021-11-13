a9f97f4a4d06e1720aa3f1312d1fd051749a7f30
adriangohjw <adriangohjw@gmail.com>
2021-11-13 16:42:51 +0800

    Fix wrong df used in df_generator

--- tf-idf/df_generator.py
+++ tf-idf/df_generator.py
@@ -3,24 +3,29 @@ import operator
 
 class DocumentProcessor:
   def call(self, df, colname):
-    temp_dict = {}
-    for cell in list(df[colname]):
-      for word in cell.split():
-        if word in temp_dict:
-          temp_dict[word] += 1
-        else:
-          temp_dict[word] = 1
-
-    sorted_temp_dict = self.sort_dict_by_value(temp_dict)
+    temp_dict = self.__get_document_frequency(df, colname)
+    sorted_temp_dict = self.__sort_dict_by_value(temp_dict)
     document_count = len(df.index)
-    return self.add_inverse_document_frequency_to_dict(sorted_temp_dict, document_count)
+    return self.__add_inverse_document_frequency_to_dict(sorted_temp_dict, document_count)
+  
+  
+  def __get_document_frequency(self, df, colname):
+    result = {}
+    for cell in list(df[colname]):
+      for word in set(cell.split()):
+        if word in result:
+          result[word] += 1
+        else:
+          result[word] = 1
+    
+    return result
     
 
-  def sort_dict_by_value(self, my_dict):
-    return dict(sorted(my_dict.items(), key=operator.itemgetter(1),reverse=True))
+  def __sort_dict_by_value(self, my_dict):
+    return dict(sorted(my_dict.items(), key=operator.itemgetter(1), reverse=True))
 
 
-  def add_inverse_document_frequency_to_dict(self, df_dict, document_count):
+  def __add_inverse_document_frequency_to_dict(self, df_dict, document_count):
     result = {}
     for key, value in df_dict.items():
       result[key] = {
